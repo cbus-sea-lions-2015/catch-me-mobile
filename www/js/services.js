5d53ab0442cd1b1f29dd5bededf9ff909d5bdd1a
msomji <maisam.somji@gmail.com>
2015-04-19 12:59:26 -0400

    automated geolocation post to database

--- www/js/services.js
+++ www/js/services.js
@@ -3,4 +3,26 @@ angular.module('starter.services', [])
 .factory("Auth", function() {
 
 
+
+
 });
+
+
+.factory('ClockSrv', function($interval){
+  var clock = null;
+  var service = {
+    startClock: function(fn){
+      if(clock === null){
+        clock = $interval(fn, 5000);
+      }
+    },
+    stopClock: function(){
+      if(clock !== null){
+        $interval.cancel(clock);
+        clock = null;
+      }
+    }
+  };
+
+  return service;
+})
