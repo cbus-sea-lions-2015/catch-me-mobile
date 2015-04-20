1dd39915e3fc54f264e2857dc94629348d2c1f30
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-20 15:18:24 -0400

    add logout

--- www/js/controllers/login_controller.js
+++ www/js/controllers/login_controller.js
@@ -1,35 +1,3 @@
-// starter.controller('LoginCtrl', function (store, $scope, $location, auth) {
-//   $scope.login = function() {
-//     // console.log('login');
-
-//     var something = auth.signin({
-//       authParams: {
-//         scope: 'openid offline_access',
-//         device: 'Mobile device'
-//       }
-//     }
-//   //   , function(profile, token, accessToken, state, refreshToken) {
-//   //     // Success callback
-//   //     console.log('success');
-//   //     store.set('profile', profile);
-//   //     store.set('token', token);
-//   //     store.set('refreshToken', refreshToken);
-//   //     $location.path('/app/profile'); ///app/profile
-//   //     // $state.go('app.profile');
-//   //   }, function() {
-//   //     // Error callback
-//   //     console.log('failure');
-//   //   });
-//   // }
-//   console.log(something);
-//   $scope.logout = function() {
-//     auth.signout();
-//     store.remove('profile');
-//     store.remove('token');
-//   }
-
-// })
-
 starter.controller('LoginCtrl', function (store, $scope, $location, auth) {
   $scope.login = function() {
     auth.signin({
@@ -38,15 +6,11 @@ starter.controller('LoginCtrl', function (store, $scope, $location, auth) {
         device: 'Mobile device'
       }
     }, function(profile, token, accessToken, state, refreshToken) {
-      // Success callback
       store.set('profile', profile);
       store.set('token', token);
       store.set('refreshToken', refreshToken);
       $location.path('/app/profile');
     }, function(error) {
-      // Error callback
-      console.log('Error');
-      console.log(error);
     });
   }
 
@@ -55,6 +19,9 @@ starter.controller('LoginCtrl', function (store, $scope, $location, auth) {
     console.log('signout');
     store.remove('profile');
     store.remove('token');
+    // store.remove('refreshToken');
+    // $location.path('/app/profile');
+    $location.path('/app/courses/new');
   }
 })
 
